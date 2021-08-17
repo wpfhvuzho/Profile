672b4000d2da7da94d1aa8020d137f44247ec745
Ryu Takahashi <85612232+230474-takahashi@users.noreply.github.com>
2021-08-17 10:32:35 +0900

    complete ProfileApp4

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileCommentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        profileImageView.isHidden = true
+        profileCommentLabel.isHidden = true
     }
 
-
+    @IBAction func tapButton1() {
+        profileCommentLabel.isHidden = false
+        profileImageView.isHidden = false
+    }
 }
 
